5c32fb3cbfd9bdab9fb955a801831829cc1aa1fa
Himanshu <manshu3220@gmail.com>
2025-08-04 13:00:16 +0000

    .

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,9 +42,9 @@ vim.o.clipboard = "unnamedplus"
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-map('n', '<leader>o', ':update<cr> :source<cr>')
-map('n', '<leader>w', ':write<cr>')
-map('n', '<leader>q', ':quit<cr>')
+map('n', '<leader>o', '<cmd>update<cr> <cmd>source<cr>')
+map('n', '<leader>w', '<cmd>write<cr>')
+map('n', '<leader>q', '<cmd>quit<cr>')
 
 -- Keep search results in center
 map("n", "n", "nzzzv", opts)
@@ -54,35 +54,38 @@ map("n", "N", "Nzzzv", opts)
 map("n", "<C-d>", "<C-d>zz", opts)
 map("n", "<C-u>", "<C-u>zz", opts)
 -- move lines up or down
-map('n', '<A-k>', ":m .-2<CR>==")
-map('i', '<A-k>', "<esc>:m .-2<CR>==gi")
-map('v', '<A-k>', ":m '<-2<CR>gv=gv")
-map('n', '<A-j>', ":m .+1<CR>==")
-map('i', '<A-j>', "<esc>:m .+1<CR>==gi")
-map('v', '<A-j>', ":m '>+1<CR>gv=gv")
+map('n', '<A-k>', "<cmd>m .-2<CR>==")
+map('i', '<A-k>', "<esc><cmd>m .-2<CR>==gi")
+map('v', '<A-k>', "<cmd>m '<-2<CR>gv=gv")
+map('n', '<A-j>', "<cmd>m .+1<CR>==")
+map('i', '<A-j>', "<esc><cmd>m .+1<CR>==gi")
+map('v', '<A-j>', "<cmd>m '>+1<CR>gv=gv")
 
 -- press jk fast to exit insert mode
 map("i", "jk", "<esc>", opts)
 map("i", "kj", "<esc>", opts)
 map("n", "x", '"_x', opts)
 
+-- Esc to remove highlight
+map("n", "<esc>", '<cmd>noh<cr>', opts)
+
 -- select all, copy, save & format
 map("n", "<c-a>", "gov<s-g>$")
-map("n", "<c-c>", ":%y+<cr>", opts)
+map("n", "<c-c>", "<cmd>%y+<cr>", opts)
 map({ "i", "n" }, "<C-s>", "<Esc><cmd>w<CR><cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
 
 -- buffers
-map("n", "<tab>", ":bnext<cr>", opts)
-map("n", "<s-tab>", ":bprevious<cr>", opts)
-map("n", "<leader>x", ":bdelete!<cr>", opts)
+map("n", "<tab>", "<cmd>bnext<cr>", opts)
+map("n", "<s-tab>", "<cmd>bprevious<cr>", opts)
+map("n", "<leader>x", "<cmd>bdelete!<cr>", opts)
 
-map({ 'n', 'v', 'x' }, '<leader>y', 'yyp<cr>')
+map({ 'n', 'v', 'x' }, '<leader>y', '"1yy"1p<cr>')
 
 map("n", "<c-j>", function() vim.diagnostic.goto_next() end, opts)
 
-map('n', '<leader>f', ":Pick files<cr>")
-map('n', '<leader>h', ":Pick help<cr>")
-map('n', '<leader>e', ":Oil<cr>")
+map('n', '<leader>f', "<cmd>Pick files<cr>")
+map('n', '<leader>h', "<cmd>Pick help<cr>")
+map('n', '<leader>e', "<cmd>Oil<cr>")
 map('n', '<leader>lf', vim.lsp.buf.format)
 
 -- keymap for find and replace
